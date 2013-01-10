464b70809b3c9213c6c1dba1e63657772441b869
jpotts18 <jeff.potter6@gmail.com>
2013-01-09 20:39:08 -0700

    adding git ignore and new config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 
-// Server Globals
-exports.MaxRoomUsers = 3;
-exports.RecentChatCount = 10;
-
 // database configuration
 exports.dbName = 'json_cms';
-exports.dbUser = 'root';
-exports.dbPassword = 'not4anyone';
+exports.dbUser = 'mysqlusername';
+exports.dbPassword = 'nottellingyou';
